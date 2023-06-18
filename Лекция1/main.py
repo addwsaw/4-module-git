1f96640bff6a46ed2beac2470c0432b8590baa60
addwsaw <artyomsolonenko11@gmail.com>
2023-06-18 13:18:36 +0300

    Функция проверяет на равенство обычный и реверснутый варианты строки

--- Лекция1/main.py
+++ Лекция1/main.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+def func(str):
+    liststr = list(str)
+    revstr = liststr[::-1]
+    if liststr != revstr:
+        return False
+    else:
+        return True
+
+
+print(func('tenet'))
+print(func('helloworld'))
