05e91e5a9fd647fa13d08f9447b9fb14d76106d1
addwsaw <artyomsolonenko11@gmail.com>
2023-07-22 14:30:12 +0300

    commit

--- Django/Django/app_Django/urls.py
+++ Django/Django/app_Django/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement_post, login, profile, register
 
-urlpatterns =[
+urlpatterns = [
     path('index/', index, name='main-page'),
     path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='advertisement-post'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile'),
+    path('register/', register, name='register'),
 ]
