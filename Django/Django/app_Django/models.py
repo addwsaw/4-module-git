d28a4f9a6e2945ffb997f13fdb650512e71cea5c
addwsaw <artyomsolonenko11@gmail.com>
2023-08-01 15:21:15 +0300

    commit

--- Django/Django/app_Django/models.py
+++ Django/Django/app_Django/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Advertisements(models.Model):
+
+    title = models.CharField('Загаловок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=13, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Если торг уместен, выберете "да"')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"Advertisements(id={self.id}, title={self.title}, price={self.price})"
+    class Meta:
+        db_table = 'Advertisements'
